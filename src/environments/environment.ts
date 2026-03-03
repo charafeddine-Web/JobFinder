2d0a172d9716f9cce57a5e64e5c8fe7f3788ef73
charafeddine-Web <charafeddinetbibzat@gmail.com>
2026-03-03 22:04:38 +0000

    chenger url de backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000',
+    apiUrl: 'https://jobfinder-api-production.up.railway.app',
     jobApiUrl: 'https://www.arbeitnow.com/api/job-board-api'
 };
