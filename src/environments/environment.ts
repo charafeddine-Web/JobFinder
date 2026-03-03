043b003267405bef29dfe470174af2fe02ac7656
charafeddine-Web <charafeddinetbibzat@gmail.com>
2026-03-03 22:21:57 +0000

    fixer bug in urlbackend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://jobfinder-api-production.up.railway.app',
+    apiUrl: 'https://jobfinder-api-production.up.railway.app/users',
     jobApiUrl: 'https://www.arbeitnow.com/api/job-board-api'
 };
